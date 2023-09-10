59f59792697292bff2d7668fa884972fb4ccedda
Eido Askayo <eido.askayo@gmail.com>
2023-09-10 12:16:37 +0300

    Change respondent answer endpoint to 1 respondent

--- QuestionnaireService/Controllers/RespondentAnswerController.cs
+++ QuestionnaireService/Controllers/RespondentAnswerController.cs
@@ -20,12 +20,23 @@ public class RespondentAnswerController : ControllerBase
     [HttpGet]
     [ProducesResponseType(typeof(RespondentAnswerDto), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public ActionResult<IEnumerable<RespondentAnswerDto>> GetRespondentAnswers()
+    public ActionResult<IEnumerable<RespondentAnswerDto>> GetRespondentAnswers(int respondentId)
     {
+        var respondent = _context.Respondents
+            .Include(respondent => respondent.RespondentAnswers!)
+            .ThenInclude(respondentAnswer => respondentAnswer.Question)
+            .SingleOrDefault(respondent => respondent.Id == respondentId);
+
+        if (respondent == null)
+        {
+            return NotFound();
+        }
+        
         return _context.RespondentsAnswers
             .Include(respondentAnswer => respondentAnswer.Respondent)
             .Include(respondentAnswer => respondentAnswer.Question)
             .Include(respondentAnswer => respondentAnswer.Answer)
+            .Where(respondentAnswer => respondentAnswer.Respondent!.Id == respondent.Id)
             .Select(respondentAnswer => new RespondentAnswerDto(respondentAnswer))
             .ToList();
     }
